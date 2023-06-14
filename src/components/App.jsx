960b88150db67e7e4a6a79838744ca218a2f38ac
Mike Merzliakov <2281205@gmail.com>
2023-06-14 06:22:02 +0300

    init proj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div> dyn.dev</div>
+    </header>
+  </div>
   );
 };
