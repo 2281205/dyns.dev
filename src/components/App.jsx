e981004a2ee7bb2edf77a6e8f02774aaf463f8bc
Mike Merzliakov <2281205@gmail.com>
2023-06-18 18:25:05 +0300

    add root permision

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ export const App = () => {
     <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <div> dyn.dev</div>
+      <div> dyns.dev</div>
     </header>
   </div>
   );
